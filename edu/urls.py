03258d4cafd466ae3b29388fa24796b5c55260b1
Le Nhat Anh <lenhatanh86@gmail.com>
2015-09-21 22:02:59 +0700

    Template and all views

--- edu/urls.py
+++ edu/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index$', views.index, name='home'),
+    url(r'^setup$', views.setup, name='setup'),
+    url(r'^database$', views.database, name='database'),
+    url(r'^template$', views.template, name='template'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "edu/index.html")
+
+
+def setup(request):
+    return render(request, "edu/setup.html")
+
+
+def database(request):
+    return render(request, "edu/database.html")
+
+
+def template(request):
+    return render(request, "edu/template.html")
